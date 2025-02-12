87d3404300492c731476531f5be9693634221d37
Pani62172 <pani62172@gmail.com>
2025-02-12 13:40:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 document.querySelectorAll(".color-btn").forEach(button => {
     button.addEventListener("click", () => {
-        let color = button.classList[1]; // Get the color from class name
-        document.body.style.backgroundColor = color; // Change background
-
-        // Reset all buttons
+        let color = button.classList[1]; 
+        document.body.style.backgroundColor = color;
         document.querySelectorAll(".color-btn").forEach(btn => {
             btn.classList.remove("active");
             btn.style.color = "white";
             btn.style.backgroundColor = btn.classList[1];
         });
-
-        // Change clicked button style
         button.classList.add("active");
         button.style.color = color;
     });
